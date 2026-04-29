c90bcc7300b69ad9252ae478288b15e386bb2a87
austinbediako <austinbediako4@gmail.com>
2026-04-29 15:31:46 +0000

    fix: bind to 0.0.0.0 and add crash handlers for Render deploy

--- server/server.js
+++ server/server.js
@@ -6,10 +6,20 @@ const { seedCrypto } = require('./utils/seedCrypto');
 
 const PORT = process.env.PORT || 5000;
 
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled rejection:', err.message);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught exception:', err.message);
+  process.exit(1);
+});
+
 connectDB().then(async () => {
   await seedCrypto();
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
   priceSimulator.start();
 });
