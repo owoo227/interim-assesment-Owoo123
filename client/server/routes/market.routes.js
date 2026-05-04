9ef332ee267ed6e3c9c8f93442fb75bcc81d335f
Austin Bediako <austinbediako4@gmail.com>
2026-05-04 18:11:41 +0000

    Delete client/server directory

--- client/server/routes/market.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getPrices } = require('../controllers/market.controller');
-
-const router = express.Router();
-
-router.get('/prices', getPrices);
-
-module.exports = router;
